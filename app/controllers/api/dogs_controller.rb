115eaa477d4c62ac75ff7a0f5da1fc68845c328d
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-01-26 19:20:15 -0500

    dog create action if logged in only

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,12 +1,16 @@
 class Api::DogsController < ApplicationController
   def create
     @dog = Dog.new(
+      user_id: current_user.id,
       name: params[:name],
       age: params[:age],
       breed: params[:breed]
-    )
-    @dog.save
-    render "show.json.jb"
+      )
+    if @dog.save
+      render "show.json.jb"
+    else
+      render json: { message: @dog.errors.full_messages }, status: 422
+    end
   end
 end
 
--- db/migrate/20210126172218_dogs_change_age_to_integer.rb
+++
@@ -1,5 +0,0 @@
-class DogsChangeAgeToInteger < ActiveRecord::Migration[6.0]
-  def change
-    change_column :dogs, :age, :integer
-  end
-end
